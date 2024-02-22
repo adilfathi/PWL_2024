455c847ede92d6a076808dfa1cb026d6311ded46
Adil Fathi <54827419+adilfathi@users.noreply.github.com>
2024-02-22 14:07:44 +0700

    Mempelajari mengenai Basic Route, Routes Parameter, serta Optional Parameter

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Praktikum Basic Routing
+Route::get('/hello', function () {
+    return 'Hello world';
+});
+
+Route::get('/world', function () {
+    return 'world';
+});
+
+// Praktikum Routes Parameter
+
+Route::get('/user/{name}', function($name){
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Post ke- '.$postId. " Komentar ke -:".$commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel ke- '.$id;
+});
+
+//Optional Parameter 
+
+Route::get('/users/{name?}', function($name=null){
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/usersa/{name?}', function($name="Adil"){
+    return 'Nama Saya '.$name;
+});
+
